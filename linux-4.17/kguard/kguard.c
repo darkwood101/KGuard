e8648bdfc53ae524f7969b2c1b8404e4286b273b
Dimitrije Pavlov <dpavlov@college.harvard.edu>
2021-11-19 18:07:15 -0500

    Kill process upon calling sys_kguard

--- linux-4.17/kguard/kguard.c
+++ linux-4.17/kguard/kguard.c
@@ -1,7 +1,16 @@
 #include <linux/kernel.h>
+#include <linux/signal.h>
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
 
 asmlinkage long sys_kguard(void) {
+    struct task_struct* t;
+    struct pid* p;
+
     printk("Called kguard\n");
-    return 4536765;
+    t = get_current();
+    p = get_task_pid(t, PIDTYPE_PID);
+    kill_pid(p, SIGKILL, 1);
+    return 0;
 }
 
